bd83f46a35cf61d37146b1c2c10ad3b03b08c39e
mschwarzer <malte.schwarzer@campus.tu-berlin.de>
2015-08-18 23:05:21 +0200

    argv

--- bot.js
+++ bot.js
@@ -1,19 +1,22 @@
 /**************
 
-USAGE: nodejs bot.js -d <working-dir> -n <number-of-actors>
+USAGE: nodejs bot.js [-d WORKING-DIR] [-n NUMBER-OF-ACTORS] [--debug]
 
 **************/
 var imdbscraper = require('./lib/imdbscraper'),
-    logger = require('./lib/logger'),
-    names = require('./names.json');
+    logger = require('./lib/logger');
 
-logger.debugLevel = 'debug';
+// Parse arguments
+var argv = require('minimist')(process.argv.slice(2));
 
+// Logger
+logger.debugLevel = (argv.debug ? 'debug' : 'info');
+
+// Get images
 var bot = new imdbscraper();
 
-bot.setNames(names);
-bot.setWorkingDir(__dirname + '/workingdir');
-bot.downloadActorImagesBornToday(5);
+bot.setNamesPath(__dirname + '/names.json');
+bot.setWorkingDir(argv.d || __dirname + '/workingdir');
+bot.downloadActorImagesBornToday(argv.n || 5);
 
-
-console.log("done " + __dirname);
+//console.log("done " + logger.debugLevel + "; " +  __dirname);
